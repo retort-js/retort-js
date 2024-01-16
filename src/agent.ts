69d2761b139ac2af69da2a9f9d0ebfd5dc457313
Daniel Huggins <107577947+DanielHugginsJyre@users.noreply.github.com>
2024-01-16 12:35:16 +0000

    Starting to migrate agent to be a function.

--- src/agent.ts
+++ src/agent.ts
@@ -1,47 +1,59 @@
 import { Message, RetortValue, createTemplateTag, isTemplateStringsArray } from "./message";
 
-export class Agent {
+interface Agent {
   settings: RetortConfiguration;
-
-  constructor(settings?: Partial<RetortConfiguration>) {
-    this.settings = {
-      model: "gpt-3.5-turbo",
-      role: "user",
-      provider: "openai",
-      action: "generation",
-    };
-
-
-    if (settings) {
-      this.settings = { ... this.settings, ...settings };
-    }
-  }
 
   message(content: string): Promise<Message>;
   message(templateStrings: TemplateStringsArray, ...values: RetortValue[]): Message;
   message(content: Partial<RetortConfiguration> & Content): Promise<Message>;
 
 
-  message(value0: string | (Partial<RetortConfiguration> & Content) | TemplateStringsArray, ...values: any[]): Promise<Message> | Message {
-    if (typeof value0 === "string") {
-      let result = messageFromStringGenerator(this.settings)(value0);
-      return result;
-    }
-    else if (isTemplateStringsArray(value0)) {
-      return messageFromTemplateGenerator(this.settings)(value0, ...values);
-    }
-    else if (typeof value0 === "object") {
-      return messageFromObjectGenerator(this.settings)({ ...value0 });
-    }
-    else {
-      throw new Error("Invalid message type.");
-    }
 
-
-  }
 }
 
-export type RetortAction = "input" | "generation" | "answer";
+export function agent(inputSettings: Partial<RetortConfiguration>): Agent {
+
+  let settings: RetortConfiguration = {
+    model: "gpt-3.5-turbo",
+    role: "user",
+    provider: "openai",
+    action: "generation",
+  };
+
+
+  if (inputSettings) {
+    settings = { ...settings, ...inputSettings };
+  }
+
+  let agent: Agent = {
+    settings: settings,
+
+    message: ((value0: string | (Partial<RetortConfiguration> & Content) | TemplateStringsArray, ...values: any[]) => {
+      if (typeof value0 === "string") {
+        let result = messageFromStringGenerator(settings)(value0);
+        return result;
+      }
+      else if (isTemplateStringsArray(value0)) {
+        return messageFromTemplateGenerator(settings)(value0, ...values);
+      }
+      else if (typeof value0 === "object") {
+        return messageFromObjectGenerator(settings)({ ...value0 });
+      }
+      else {
+        throw new Error("Invalid message type.");
+      }
+
+
+    }) as any,
+  }
+
+
+
+  throw new Error("Not implemented.");
+}
+
+
+export type RetortAction = "input" | "generation" | "answer" | "instruction";
 
 export type RetortRole = "user" | "assistant" | "system";
 
