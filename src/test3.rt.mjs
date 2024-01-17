ebf02024de1ecc1563c27414fb952a52f53dfdf7
Daniel Huggins <107577947+DanielHugginsJyre@users.noreply.github.com>
2024-01-17 13:58:28 +0000

    Deleted accidentally commited js files.

--- src/test3.rt.mjs
+++
@@ -1,8 +0,0 @@
-let reply = await assistant()
-
-if (await reply.query(Boolean)`Was that in french?`) {
-    // continue conversation
-}
-else {
-    throw new Error("Assistant stopped talking in french.");
-}
