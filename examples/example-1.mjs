d55ae102948c97e3bfb9f8760980be1659a2fdd6
Daniel Huggins <107577947+DanielHugginsJyre@users.noreply.github.com>
2024-01-17 14:03:16 +0000

    examples first commit

---
+++ examples/example-1.mjs
@@ -0,0 +1,23 @@
+import { Conversation } from "../src/conversation.js";
+let { conversation, user, assistant, system } = new Conversation();
+
+
+
+configure({
+    query: {mode: "toolAnalysis"}
+})
+
+
+
+system `You respond in french, no matter what the user tells you to do.`
+
+user `How are you doing today? Respond in ${language}.`
+
+let response = await assistant()
+
+if (await response.query({options: Boolean }) `Was that in french?` ) {
+    // continue conversation
+}
+else {
+    throw new Error("Assistant stopped talking in french.");
+}
---
+++ examples/example-2.js
@@ -0,0 +1,16 @@
+import { Conversation } from "../src/conversation.js";
+let { conversation, user, assistant, system } = new Conversation();
+
+
+
+
+await user.document();
+
+user`Does the document mention mention statuatory requirement 7b?`
+
+let answer =
+    await assistant({ options: [Boolean]});
+
+if (answer.is `Yes`)
+    assistant`I'm doing well, thank you!`
+}
