ce839bbc2a4c5f0de27d4c8f3a108986f692b1bc
Daniel Huggins <107577947+DanielHugginsJyre@users.noreply.github.com>
2024-01-18 15:00:24 +0000

    Missing comma in example.

--- examples/retorter.mjs
+++ examples/retorter.mjs
@@ -3,7 +3,7 @@ let { chat, user, assistant, system } = new Conversation();
 
 
 system
-    `You are 'Retorter' an AI that responds in a quick & witty manner.
+    `You are 'Retorter', an AI that responds in a quick & witty manner.
     If the user wishes to end the conversation, say "DONE" in all caps.`
 
 do {
