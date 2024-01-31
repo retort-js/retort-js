7d3d5877625e26823f8af8557f89a0cd546dd573
Daniel Huggins <107577947+DanielHugginsJyre@users.noreply.github.com>
2024-01-31 15:21:04 +0000

    Wildcard module definition for globals.ts

--- src/globals.ts
+++ src/globals.ts
@@ -1,6 +1,13 @@
 import type { Conversation } from "./conversation";
+import type { RetortScript } from "./script";
 
 declare global {
     const $: Conversation; // Use 'any' type or a more specific type or interface.
 }
 
+// Declare a wildcard module for ".rt." files. 
+// You're only allowed to declare these in .d.ts files - but to get it to copy to the output directory, we just ignore this error.
+// @ts-expect-error
+declare module "*.rt.*" {
+    export = RetortScript;
+}
