b558b55417079b333f5fa7c3436287fb791c4b12
Daniel Huggins <107577947+DanielHugginsJyre@users.noreply.github.com>
2024-01-31 12:23:41 +0000

    Retort script function typing

--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,15 @@
 import { Conversation } from "./conversation";
 import { id } from "./id";
 
-export function script<T>(chatFunction: ChatFunction<T>) {
+type Tuple = unknown[];
 
-    let run = (... values: any[]) => {
+export interface RetortScript<T extends Tuple> {
+    run: (...values: T) => any;
+}
+
+export function script<T extends Tuple>(chatFunction: ChatFunction<T>): RetortScript<T> {
+
+    let run = (... values: T) => {
 
         const conversation = new Conversation();
 
@@ -20,7 +26,7 @@ export function script<T>(chatFunction: ChatFunction<T>) {
     // Only run the chat function if this module is the main module.
     setTimeout(() => {
         if (returnedModule.__retortChatFunctionId === require.main?.exports?.__retortChatFunctionId) {
-            returnedModule.run();
+            (returnedModule.run as any)();
         }
     }, 0);
 
@@ -30,4 +36,6 @@ export function script<T>(chatFunction: ChatFunction<T>) {
 
 }
 
-type ChatFunction<T> = ($: Conversation, ...values: any[]) => T;
+script(($, x: string) => {}).run("hello");
+
+type ChatFunction<T extends Tuple> = ($: Conversation, ...values: T) => any;
