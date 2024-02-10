7cdb1f6e8db8d4e4e8d82849b280b24b5b6505a8
Daniel Huggins <107577947+DanielHugginsJyre@users.noreply.github.com>
2024-02-10 13:02:39 +0000

    Added line continuations and escape sequences like newlines and tabs. Needs testing

--- src/message.ts
+++ src/message.ts
@@ -28,6 +28,9 @@ export function createTemplateTag(settings: RetortConfiguration) {
         // Remove carriage returns from strings.
         strings = strings.map(str => str.replace(/\r/g, ""));
 
+        // Allow line continuations
+        strings = strings.map(str => str.replace(/(?<!\\)\\[^\S\n]\n/g, ""));
+
         // Remove any whitespace at the start of a line in each of the strings - except that which is explicitly specified.
         strings = strings.map(str => str.replace(/\n[^\S\r\n]+/g, "\n"));
 
@@ -40,10 +43,8 @@ export function createTemplateTag(settings: RetortConfiguration) {
         // Remove trailing whitespace from the last string, if any.
         strings[strings.length - 1] = (strings[strings.length - 1] || "").trimEnd();
 
-        // TODO: Allow \ to begin and end a line, to allow for indentation.
-
         // Now, finally, encode the strings.
-        strings = strings.map(str => String(str));
+        strings = strings.map(str => unescape(str));
 
 
 
@@ -64,4 +65,58 @@ export function createTemplateTag(settings: RetortConfiguration) {
 
 export function isTemplateStringsArray(templateStrings: TemplateStringsArray | unknown): templateStrings is TemplateStringsArray {
     return Array.isArray(templateStrings) && Array.isArray((templateStrings as any).raw);
+}
+
+export function unescape(str: string) {
+    let segments = str.split(/(\\\x[a-fA-F0-9]{2}|\\\u[a-fA-F0-9]{4}|\\\u\{[a-fA-F0-9]{1,6}\}|\\.)/g).filter(x => x);
+
+    return segments.map(unescapeSegment).join("");
+}
+
+export function unescapeSegment(str: string) {
+    if (str.startsWith("\\")) {
+        if (str[1] === "x") {
+
+            // Use a regex to check that this is a valid escape sequence
+            if (!/\\x[a-fA-F0-9][a-fA-F0-9]/.test(str)) {
+                throw new Error(`Malformed Latin-1 escape sequence; should be like "\\x00"`);
+            }
+
+            return String.fromCharCode(parseInt(str.slice(2), 16));
+        } else if (str[1] === "u") {
+            if (str[2] === "{") {
+
+                // Use a regex to check that this is a valid escape sequence
+                if (!/\\u\{[a-fA-F0-9]{1,6}\}/.test(str)) {
+                    throw new Error(`Malformed unicode escape sequence; should be like "\\u{000000} or "\\u0000`);
+                }
+
+                return String.fromCharCode(parseInt(str.slice(3, -1), 16));
+            } else {
+
+                // Use a regex to check that this is a valid escape sequence
+                if (!/\\u[a-fA-F0-9]{4}/.test(str)) {
+                    throw new Error(`Malformed unicode escape sequence; should be like "\\u{000000} or "\\u0000`);
+                }
+
+                return String.fromCharCode(parseInt(str.slice(2, 6), 16));
+            }
+        } else if (str[1] === "n") {
+            return "\n";
+        } else if (str[1] === "r") {
+            return "\r";
+        } else if (str[1] === "t") {
+            return "\t";
+        } else if (str[1] === "\\") {
+            return "\\";
+        }
+        else if (str[1] === "s") {
+            // Special space escaping behaviour.
+            return " ";
+        } else {
+            throw new Error(`Unsupported escape sequence: "${str}"`)
+        }
+    } else {
+        return str;
+    }
 }
