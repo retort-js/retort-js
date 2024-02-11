08e28ae5a2a1a7fcbd84374c2d68c0397c06f2ab
Daniel Huggins <107577947+DanielHugginsJyre@users.noreply.github.com>
2024-02-11 21:00:42 +0000

    Deleted pointless wrapping capability

--- src/run.ts
+++ src/run.ts
@@ -4,24 +4,21 @@ import { RetortScriptInProgress } from "./script";
 
 export default function run(scriptName: string) {
 
+  const conversation = new Conversation();
 
-  let x = (params?: any): RetortScriptInProgress<any> => {
-    const conversation = new Conversation();
+  async function runInner() {
+    return (await (getScriptFunc(scriptName))).__scriptFunc(conversation);
+  }
 
-    async function runInner() {
-      return (await (getScriptFunc(scriptName))).__scriptFunc(conversation);
-    }
+  let executing = runInner();
 
-    let executing = runInner();
-
-    let scriptInProgress: RetortScriptInProgress<any> = {
-      $: conversation,
-      completionPromise: executing,
-      retortType: "RetortScriptInProgress",
-    };
-
-    return scriptInProgress;
+  let scriptInProgress: RetortScriptInProgress<any> = {
+    $: conversation,
+    completionPromise: executing,
+    retortType: "RetortScriptInProgress",
   };
 
-  
+  return scriptInProgress;
+
+
 }
