028047478312507d9009bf413a261731d319d4bd
Daniel Huggins <107577947+DanielHugginsJyre@users.noreply.github.com>
2024-02-20 18:13:13 +0000

    Fixed error with retortFunctionWrapper overwriting what was there before.

--- src/extendable-function.ts
+++ src/extendable-function.ts
@@ -4,17 +4,13 @@ abstract class RetortExtendableFunction extends Function {
   constructor() {
     function _retortFunctionWrapper(): any {
       const self = _retortFunctionWrapper as any;
-      if (typeof self.__wrappedFunction !== "function") {
-        throw new Error("RetortExtendableFunction: __wrappedFunction not a function");
+      if (!self.__wrappedFunction) {
+        throw new Error("RetortExtendableFunction: __wrappedFunction not defined")
       }
       return self.__wrappedFunction.apply(_retortFunctionWrapper, arguments);
     }
 
     const self = _retortFunctionWrapper as any;
-
-    self.__wrappedFunction = 
-    (() => { throw new Error("RetortExtendableFunction: __wrappedFunction not set") }) as any;
-
 
     return Object.setPrototypeOf(self, new.target.prototype);
   }
