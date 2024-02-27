511e46bd8c43f2ec31128d0829158088ef6b22a5
Daniel Huggins <107577947+DanielHugginsJyre@users.noreply.github.com>
2024-02-27 08:31:27 +0000

    Tweaked method call names.

--- examples/script-example.js
+++ examples/script-example.js
@@ -1,10 +1,10 @@
 // This magic line allows you to use this file as both a script and a module.
-module.exports = require("../dist/script").script(async $ => {
+module.exports = require("../dist/index.js").retort(async $ => {
 
     $.system `You are 'Retorter', an AI that responds in a quick & witty manner.`
 
-    await $.user()
+    await $.user.input()
 
-    await $.assistant();
+    await $.assistant.generation();
 
 });
