a4b1bd5185754e42d592d40ac7dbae0333e68af9
AdamDCostaJyre <adam.dcosta@jyre.io>
2024-03-15 12:23:24 +0000

    made change to rebuild dist folder

--- examples/script-example.rt.js
+++ examples/script-example.rt.js
@@ -1,8 +1,8 @@
 // This magic line allows you to use this file as both a script and a module.
 module.exports = require("../dist/index.js").retort(async ($) => {
-  $.system`You are 'Retorter', an AI that responds in a quick & witty manner.`;
+  $.system`You are a 'Retorter', an AI that responds in a quick & witty manner.`;
 
   await $.user.input();
 
-  await $.assistant.generation();
+  await $.assistant.generation();                           
 });
