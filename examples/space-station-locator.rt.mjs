e0ca1e8ac7b1c5c70d27f2059b2a9830a345edbe
kylehek <kylehek99@gmail.com>
2024-03-28 16:57:52 +0000

    created space station locator example

---
+++ examples/space-station-locator.rt.mjs
@@ -0,0 +1,33 @@
+import { retort } from "../dist/index.js";
+
+// Function to fetch space station location from the Open Notify API
+const getCoordinates = async function () {
+  try {
+    const response = await fetch(
+      'https://api.wheretheiss.at/v1/satellites/25544'
+    );
+    const data = await response.json();
+    return [data.longitude, data.latitude];
+  } catch (err) {
+    console.log("Couldn't fetch coordinates", err);
+    throw err;
+  }
+};
+
+const retorter = retort(async ($) => {
+  let coordinates = await getCoordinates()
+  const instructions = `
+  You are an expert on the Open Notify Recipe API. Using the location of the ISS, please go into detail on where it is currently located.
+  Here is the link to the documentation: https://wheretheiss.at/w/developer
+  You should respond with the current location in the world based on these coordinates: ${coordinates}.
+  Respond with the location of the coordinates as in Country, be exact.
+  Answer any question about the current location.`
+  
+  $.system(instructions);
+
+  $.user`Where is the ISS currently?`
+
+  await $.assistant.generation();
+});
+
+retorter._run(); // Run the conversation
