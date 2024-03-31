94cab11379c26fef4d2dda67cbba2e680fdfdc96
Daniel Huggins <107577947+DanielHugginsJyre@users.noreply.github.com>
2024-03-31 18:48:48 +0100

    Hid unused method on conversation, and tweaked the way fromObject works.

--- src/conversation.ts
+++ src/conversation.ts
@@ -14,8 +14,10 @@ export interface RetortScriptImport<T> {
 
 export class RetortConversation extends RetortExtendableFunction {
     readonly id = id("cnv");
-    readonly chat = this;
-    readonly messagePromises: (RetortMessage | Promise<RetortMessage>)[] = [];
+    /**
+     * @deprecated
+     */
+    readonly chat = this; 
 
     protected get __wrappedFunction() {
         return this.prompt;
@@ -29,40 +31,65 @@ export class RetortConversation extends RetortExtendableFunction {
 
     run = run;
 
+    /**
+     * @deprecated
+     * @see settings.model
+     */
     get model() {
         return this.settings.model;
     }
 
+    /**
+     * @deprecated
+     * @see settings.model
+     */
     set model(value: string) {
         this.settings.model = value;
     }
 
+    /**
+     * @deprecated
+     * @see settings.temperature
+     */
     get temperature() {
         return this.settings.temperature;
     }
 
+
+    /**
+     * @deprecated
+     * @see settings.temperature
+     */
     set temperature(value: number) {
         this.settings.temperature = value;
     }
 
+    /**
+     * @deprecated
+     * @see settings.topP
+     */
     get topP() {
         return this.settings.topP;
     }
 
+    /**
+     * @deprecated
+     * @see settings.topP
+     */
     set topP(value: number) {
         this.settings.topP = value;
     }
 
+    readonly messages: RetortMessage[] = [];
 
-    get messages(): RetortMessage[] {
-        for (let m of this.messagePromises) {
-            console.log("messagePromises", m, this.messagePromises)
-            if (!(m instanceof RetortMessage)) {
-                throw new Error("Cannot access messages until all promises have resolved.");
-            }
-        }
-        return this.messagePromises as RetortMessage[];
+    /**
+     * Use the promise property of each message to get an array of promises
+     * @deprecated
+     */
+    get messagePromises() {
+        return this.messages.map((m) => m.promise);
     }
+
     user = agent(this, "user");
     assistant = agent(this, "assistant");
     system = agent(this, "system");
@@ -75,7 +102,7 @@ export class RetortConversation extends RetortExtendableFunction {
         return defineGeneration(this, "assistant", false);
     }
 
-    get prompt() {
+    private get prompt() {
         return definePrompt(this, "user", false);
     }
 
@@ -91,7 +118,7 @@ export class RetortConversation extends RetortExtendableFunction {
     static fromObject(obj: SerializableRetortConversation) {
         const conversation = new RetortConversation();
         conversation.settings = obj.settings;
-        conversation.messagePromises.push(...obj.messages);
+        conversation.messages.push(...obj.messages);
         return conversation;
     }
 }
