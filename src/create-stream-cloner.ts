4755cd9207d41284d4246047c487de5fdd6b7dad
Daniel Huggins <107577947+DanielHugginsJyre@users.noreply.github.com>
2024-03-31 17:29:17 +0100

    create-stream-cloner

---
+++ src/create-stream-cloner.ts
@@ -0,0 +1,27 @@
+let Done = Symbol("retort-stream-done");
+export default function createStreamCloner<T>(generator: AsyncGenerator<T>) {
+
+  let buffer: (Promise<T | typeof Done>)[] = [];
+  async function iterate() {
+    let resolve: (value: T | typeof Done) => void;
+    let promise = new Promise<T | typeof Done>((r) => resolve = r);
+    buffer.push(promise);
+    for await (const chunk of generator) {
+      resolve!(chunk)
+    }
+    resolve!(Done);
+  }
+
+  return async function* getStream<T>() {
+    for (let i = 0; i < buffer.length; i++) {
+      let value = await buffer[i];
+      if (value === Done) {
+        return;
+      }
+      else {
+        yield value as T;
+      }
+    }
+  }
+
+}
